05aff08f6d3f1e7f123eb2c06cd0276b9ead4d3c
GroundAura <52499897+GroundAura@users.noreply.github.com>
2023-09-22 00:15:17 -0600

    fomod image and build script

--- scripts/test.bat
+++ scripts/test.bat
@@ -2,5 +2,7 @@
 title test
 cd ".."
 
-robocopy "dist\FormList Patch Collection\Base" "C:\Games\Elder Scrolls\V - Skyrim\SMEFT\mods\FormList Patch Collection (pre-release) []" /e
+robocopy "dist\FormList Patch Collection\Base" "C:\Games\Elder Scrolls\V - Skyrim\SMEFT\mods\FormList Patch Collection (pre-release) []"
+robocopy "dist\FormList Patch Collection\Base\ESP" "C:\Games\Elder Scrolls\V - Skyrim\SMEFT\mods\FormList Patch Collection (pre-release) []"
+robocopy "dist\FormList Patch Collection\Base\Patch" "C:\Games\Elder Scrolls\V - Skyrim\SMEFT\mods\FormList Patch Collection (pre-release) []"
 
